a4e541868e64f99a50e2e80f150e0253c928a5d1
Nicolas Jurquet <nicolor01@hotmail.com>
2023-02-27 21:25:31 +0100

    Configure functions and tests

--- test_utils.py
+++ test_utils.py
@@ -3,14 +3,20 @@ import utils
 
 
 def test_fact():
+    assert utils.fact(0) == 1
     assert utils.fact(2) == 2
-    assert utils.fact(3) == 6
     assert utils.fact(5) == 120
+    with pytest.raises(ValueError):
+        utils.fact(-1)
 
 
 def test_roots():
-    assert utils.roots(1, 2, -3) == (1, 3)
+    assert isinstance(utils.roots(1, 2, -3), tuple)
+    assert utils.roots(1, 2, -3) == pytest.approx((-3.0, 1.0))
+    assert utils.roots(1, 1, 1) == tuple()
+    assert utils.roots(2, 0, 0) == pytest.approx((0.0,))
+    assert utils.roots(2, 1, -2) == pytest.approx((-1.28077640, 0.78077640))
 
 
 def test_integrate():
-    assert utils.integrate("x", 0, 1) == 1
+    assert utils.integrate("x", 0, 1) == pytest.approx(0.5)
--- utils.py
+++ utils.py
@@ -1,24 +1,41 @@
-def fact(n):
+from scipy import integrate as itg
+import math
+
+
+def fact(n: int) -> int:
     """Computes the factorial of a natural number.
 
     Pre: -
     Post: Returns the factorial of 'n'.
     Throws: ValueError if n < 0
     """
-    pass
+    if n < 0:
+        raise ValueError("n must be positive")
+    if n == 0:
+        return 1
+
+    res = 1
+    for i in range(1, n+1):
+        res *= i
+    return res
 
 
-def roots(a, b, c):
+def roots(a: int, b: int, c: int) -> tuple:
     """Computes the roots of the ax^2 + bx + c = 0 polynomial.
 
     Pre: -
     Post: Returns a tuple with zero, one or two elements corresponding
             to the roots of the ax^2 + bx + c polynomial.
     """
-    pass
+    delta = b**2 - 4*a*c
+    if delta < 0:
+        return tuple()
+    if delta == 0:
+        return (-b/2/a,)
+    return ((-b - math.sqrt(delta))/2/a, (-b + math.sqrt(delta))/2/a)
 
 
-def integrate(function, lower, upper):
+def integrate(function, lower: float, upper: float) -> float:
     """Approximates the integral of a fonction between two bounds
 
     Pre: 'function' is a valid Python expression with x as a variable,
@@ -31,7 +48,7 @@ def integrate(function, lower, upper):
             you'll probably need the 'eval' function to evaluate the function
             to integrate given as a string.
     """
-    pass
+    return itg.quad(lambda x: eval(function), lower, upper)[0]
 
 
 if __name__ == '__main__':
